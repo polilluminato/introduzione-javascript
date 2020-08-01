3de560fd03c2346f1684a1eb3ab8262bcd5bead4
Alberto Bonacina <bonacina.alberto@gmail.com>
2020-08-01 15:45:24 +0200

    feat: Aggiunto codice per lo script delle variabili

--- code/js/variabili.js
+++ code/js/variabili.js
@@ -1,2 +1,76 @@
-var a = 1;
-console.table(a);
+// var a = 1;
+// console.log(a);
+
+
+
+
+//Dichiarazione di variabili
+// var b, 
+//     c = 7, 
+//     d = 'ciao';
+// console.log("b: " + b + " c: " + c + " d: " + d);
+
+
+
+
+
+//Case Sensitive
+// var lastname, lastName;
+// lastName = 'Rossi';
+// lastname = 'Bianchi';
+
+
+
+
+
+//Tipi di dato
+// var valoreACaso, valore_a_caso;
+
+// valoreACaso = 5;
+// console.log(valoreACaso + " tipo: " + typeof valoreACaso);
+
+// valoreACaso = 3.14;
+// console.log(valoreACaso + " tipo: " + typeof valoreACaso);
+
+// valoreACaso = 'unicorno';
+// console.log(valoreACaso + " tipo: " + typeof valoreACaso);
+
+// valoreACaso = "drago";
+// console.log(valoreACaso + " tipo: " + typeof valoreACaso);
+
+// valoreACaso = true;
+// console.log(valoreACaso + " tipo: " + typeof valoreACaso);
+
+// valoreACaso = {'frutto': 'avocado', costo: 4};
+// console.log(valoreACaso);
+// console.log(valoreACaso + " tipo: " + typeof valoreACaso);
+
+// valoreACaso = ['primo','secondo','terzo','quarto'];
+// console.log(valoreACaso);
+// console.log('primo elemento: ' + valoreACaso[0]);
+// console.log(valoreACaso + " tipo: " + typeof valoreACaso);
+
+
+
+
+
+//Operatori di comparazione e coercion
+var a = 5, b = '6';
+if( a < b ) {
+    console.log('la matematica non è un opinione');
+}
+console.log( a + b );
+console.log( a + Number(b) );
+
+if( '2' > '12' ) {
+    console.log('Avrà ragione JavaScript?');
+}
+
+if( false < 1 ) {
+    console.log('Avrà ragione JavaScript?');
+}
+
+var c = '5', d = 5;
+if ( c == d ) {
+    console.log('Avrà ragione JavaScript?');
+}
