48e50f3952672ec2c90dda91ee6099759b090078
Alberto Bonacina <bonacina.alberto@gmail.com>
2020-08-01 14:55:30 +0200

    Aggiunto codice per la sezione delle console

--- code/js/console.js
+++ code/js/console.js
@@ -1,1 +1,17 @@
 console.log("ciao a tutti quanti");
+
+//Tipi diversi di console
+// console.log('Tranquillo');
+// console.warn('Attenzione');
+// console.error('Aiutoooooooo!!!!!');
+
+//Console.table
+// var spesa = {'🍊':3,'🍌':5,'🍓':6,'🥑':4,'🥥':2};
+// console.log(spesa);
+
+
+//Timing dei processi
+// console.time('inizioProcedura');
+// setTimeout(()=>{
+//     console.timeEnd('inizioProcedura');
+// },3000);
