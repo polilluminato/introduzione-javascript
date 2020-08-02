4fc9ceb87686d36a2b39a0a1b15e4078bf21adae
Alberto Bonacina <bonacina.alberto@gmail.com>
2020-08-02 14:59:42 +0200

    Aggiunti esempi sul ternary operator

---
+++ code/js/ternary_operator.js
@@ -0,0 +1,14 @@
+var eta = 18;
+var isMaggiorenne = ( eta >= 18 ? true : false );
+console.log("Sono maggiorenne? " + isMaggiorenne);
+
+
+
+
+
+//Assegnamento stringa
+var altezza = 200
+var rispostaOperatore = ( altezza > 180 
+                            ? 'Puoi salire sul Blue Tornato' 
+                            : 'Non puoi salire perchè sei troppo basso');
+console.log(rispostaOperatore);
