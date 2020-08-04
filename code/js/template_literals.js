8f87cb58e6c8cfadb5b8f96086c3ed8766d1f22d
Alberto Bonacina <bonacina.alberto@gmail.com>
2020-08-04 07:17:33 +0200

    Aggiunti esempi per template literals

---
+++ code/js/template_literals.js
@@ -0,0 +1,41 @@
+// var temperatura = 149.5;
+// console.log("La temperatura è: " + temperatura + "°C");
+
+
+
+
+
+
+//Concatenazione di più stringhe
+// var costoUnitario = 2.50,  
+//         quantitaAcquistata = 5;
+// console.log("Hai comprato " + quantitaAcquistata + 
+//                 " birre al costo di " + costoUnitario + "€ l'una.\n"+
+//                 "Il tuo totale è "+ (quantitaAcquistata*costoUnitario) +"€");
+
+
+
+
+
+//Template literals
+// console.log(`Questo è un nuovo tipo di stringa`);
+
+
+
+
+
+// var costoUnitario = 2.50,  
+//         quantitaAcquistata = 5;
+// console.log(`Hai comprato ${quantitaAcquistata} birre al 
+// costo unitario di ${costoUnitario}€. 
+// Il tuo totale è ${quantitaAcquistata*costoUnitario}€`);
+
+
+
+
+
+//Template literals nesting
+// var temperatura = 145.6,
+//     unitaDiMisura = "K";
+// console.log(`La temperatura misurata è di ${temperatura} ${ (unitaDiMisura == "K" ? `gradi Kelvin (${unitaDiMisura})` : ` gradi Fahrenheit (${unitaDiMisura})` ) }`);
+// console.log(`La temperatura misurata è di ${temperatura} gradi ${ (unitaDiMisura == "K" ? `Kelvin` : `Fahrenheit` ) } (${unitaDiMisura})`);
