3243a6696c9796e14b6d68af8785f514a823d22c
Alberto Bonacina <bonacina.alberto@gmail.com>
2020-08-05 07:32:35 +0200

    Aggiunto il codice da completare per le classi

---
+++ code/js/classi.js
@@ -0,0 +1,45 @@
+// class Animale{
+//     constructor(name){
+//         this.name = name;
+//     }
+
+//     printNomeConSaluto(){
+//         console.log(`Ciao ${this.name}`);
+//     }
+// }
+// var myAnimal = new Animale('Jack');
+// console.log(myAnimal.name);
+// myAnimal.printNomeConSaluto();
+
+
+
+
+
+
+// class Rettangolo{
+
+//     #base; #altezza;
+
+//     constructor(base,altezza){
+//         this.#base = base;
+//         this.#altezza = altezza;
+//     }
+
+//     get area(){
+//         return this.#base*this.#altezza;
+//     }
+
+//     calcolaArea(){
+//         return this.#base*this.#altezza;
+//     }
+// }
+
+// var myRectangle = new Rettangolo(10,20);
+
+// //console.log(myRectangle.base);
+// console.log(myRectangle.area);
+// console.log(myRectangle.calcolaArea());
+
+
+//Fai il codice per estendere una classe
+//Vedi se parlare dei metodo privati e pubblici
