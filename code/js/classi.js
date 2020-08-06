177ef3bfa6118e20ae7c54eeac2ffc95026d6bc5
Alberto Bonacina <bonacina.alberto@gmail.com>
2020-08-06 07:12:26 +0200

    Aggiunti esempi nel codice per le classi

--- code/js/classi.js
+++ code/js/classi.js
@@ -25,21 +25,64 @@
 //         this.#altezza = altezza;
 //     }
 
+//     //Getter
 //     get area(){
 //         return this.#base*this.#altezza;
 //     }
 
+//     //Funzione
 //     calcolaArea(){
 //         return this.#base*this.#altezza;
 //     }
+//     calcolaPerimetro(){
+//         return (this.#base*2)+(this.#altezza*2);
+//     }
 // }
-
 // var myRectangle = new Rettangolo(10,20);
-
 // //console.log(myRectangle.base);
 // console.log(myRectangle.area);
 // console.log(myRectangle.calcolaArea());
 
 
-//Fai il codice per estendere una classe
+
+
+
+
+
+
+//Estendere una classe
+// class Quadrato extends Rettangolo {
+//     constructor(lato) {
+//       super(lato,lato); 
+//     }
+// }
+// var myQuadrato = new Quadrato(20);
+// console.log(myQuadrato.area);
+// console.log(myQuadrato.calcolaArea());
+// console.log(myQuadrato.calcolaPerimetro());
+
+
+
+
+
+
+
+
+
+// class Quadrato extends Rettangolo {
+//     #lato;
+//     constructor(lato) {
+//       super(lato,lato); 
+//       this.#lato = lato;
+//     }
+
+//     calcolaPerimetro(){
+//         return this.#lato*5;
+//     }
+// }
+// var myQuadrato = new Quadrato(20);
+// console.log(myQuadrato.area);
+// console.log(myQuadrato.calcolaArea());
+// console.log(myQuadrato.calcolaPerimetro());
+
 //Vedi se parlare dei metodo privati e pubblici
