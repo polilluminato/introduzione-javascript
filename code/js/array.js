c10d62f291e31b8f9f77981bd3cff88705162efd
Alberto Bonacina <bonacina.alberto@gmail.com>
2020-08-07 07:40:17 +0200

    Aggiunto file per array

---
+++ code/js/array.js
@@ -0,0 +1,80 @@
+// var myArray = [1,2,3,4]; //new Array(1,2,3,4)
+// console.log(myArray)
+
+// console.log("Lunghezza array: " + myArray.length);
+// console.log("Primo elemento: " + myArray[0]);
+// console.log("Ultimo elemento: " + myArray[myArray.length-1]);
+
+
+
+
+
+
+
+
+// var multipleTypeArray = [1,true,"Alberto",{nome:"alberto",cognome:"bonacina"}];
+// console.log(multipleTypeArray);
+
+
+
+
+
+
+
+
+// for( var i = 0 ; i < multipleTypeArray.length ; i++ ){
+//     console.log(multipleTypeArray[i]);
+// }
+// multipleTypeArray.forEach(function(single,index){
+//     console.log(index + ") " + single)
+// })
+
+
+
+
+
+
+
+
+// var operationalArray = [1,2,3,4,5,6,7];
+// console.log("Lunghezza prima: " + operationalArray.length);
+
+
+
+
+//Operazioni su array
+// operationalArray.push(8);
+// console.log("Push");
+// console.log(operationalArray);
+// console.log("Lunghezza prima: " + operationalArray.length);
+// console.log("\n");
+
+// operationalArray.pop();
+// console.log("Pop");
+// console.log(operationalArray);
+// console.log("Lunghezza prima: " + operationalArray.length);
+// console.log("\n");
+
+// operationalArray.shift();
+// console.log("Shift");
+// console.log(operationalArray);
+// console.log("Lunghezza prima: " + operationalArray.length);
+// console.log("\n");
+
+// operationalArray.unshift(1);
+// console.log("Unshift");
+// console.log(operationalArray);
+// console.log("Lunghezza prima: " + operationalArray.length);
+// console.log("\n");
+
+// operationalArray.splice(2,1); // (posizione,quanti_elementi_rimuovere)
+// console.log("Remove at index (che so)");
+// console.log(operationalArray);
+// console.log("Lunghezza prima: " + operationalArray.length);
+// console.log("\n");
+
+// operationalArray.splice(operationalArray.indexOf(4),1); // (posizione,quanti_elementi_rimuovere)
+// console.log("Remove at index (che non so)");
+// console.log(operationalArray);
+// console.log("Lunghezza prima: " + operationalArray.length);
+// console.log("\n");
