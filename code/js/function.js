44540c7107de26423fcfd0926dc0d4b7e3338b6b
Alberto Bonacina <bonacina.alberto@gmail.com>
2020-08-07 07:39:14 +0200

    Rinominato file

---
+++ code/js/function.js
@@ -0,0 +1,55 @@
+//Definizione classica
+// function saluti(){
+//     console.log("Ciao a tutti");
+// }
+// var funzioneSaluti = saluti;
+// funzioneSaluti();
+
+
+
+
+
+
+
+//Definizione compatta
+// var funzioneSaluti = function saluti(){
+//     console.log("Ciao a tutti");
+// }
+// funzioneSaluti();
+
+
+
+
+
+
+
+
+//Arrow function
+// var funzioneSaluti = ()=>{
+//     console.log("Ciao a tutti");
+// }
+// funzioneSaluti();
+
+
+
+
+
+
+
+
+//Arrow function compatta
+// var funzioneSaluti = () => console.log("Ciao a tutti");
+// funzioneSaluti();
+
+
+
+
+
+
+
+
+//Arrow function parametri
+// var funzioneSalutoConNome = (nome) => {
+//     console.log(`Ciao ${nome}`);
+// }
+// funzioneSalutoConNome('Alberto');
