828424a47dcb4aab25cd37d0128a271499ec2cc9
Alberto Bonacina <bonacina.alberto@gmail.com>
2020-09-08 07:30:21 +0200

    Inizio di codice per const, let, var

---
+++ code/js/const_let_var.js
@@ -0,0 +1,39 @@
+// const miaCostante = 3.14159;
+// console.log(miaCostante);
+// miaCostante = 2.71828;
+
+
+
+
+
+
+// var miaCostante = 3.14159;
+// console.log(miaCostante);
+// miaCostante = 2.71828;
+// console.log(miaCostante);
+
+
+
+
+
+
+
+// var i = 5;
+// console.log(i);
+// for(var i = 0 ; i < 3 ; i++) {
+//     console.log(i);
+// }
+// console.log(i);
+
+
+
+
+
+
+
+// let i = 5;
+// console.log('PRIMA) ' + i);
+// for(let i = 0 ; i < 3 ; i++) {
+//     console.log('DENTRO) ' + i);
+// }
+// console.log('DOPO) ' + i);
