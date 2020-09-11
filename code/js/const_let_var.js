1784f0416cf37a8b53ab8632a8095ac4d52f42ff
Alberto Bonacina <bonacina.alberto@gmail.com>
2020-09-11 07:31:05 +0200

    Concluso file per var, let e const

--- code/js/const_let_var.js
+++ code/js/const_let_var.js
@@ -17,13 +17,18 @@
 
 
 
-
+//Function scope di var
 // var i = 5;
-// console.log(i);
-// for(var i = 0 ; i < 3 ; i++) {
-//     console.log(i);
+// console.log('PRIMA) ' + i);
+
+// function printaLoop(){
+//     for(var i = 0 ; i < 3 ; i++){
+//         console.log('\tDENTRO) ' + i);
+//     }
 // }
-// console.log(i);
+// printaLoop();
+
+// console.log('DOPO) ' + i);
 
 
 
@@ -31,9 +36,64 @@
 
 
 
+//Block Scope di var
+// var i = 5;
+// console.log('PRIMA) ' + i);
+
+// for(var i = 0 ; i < 3 ; i++) {
+//     console.log('\t\DENTRO) ' + i);
+// }
+
+// console.log('DOPO) ' + i);
+
+
+
+
+
+
+//Esempio con block scope di let
 // let i = 5;
 // console.log('PRIMA) ' + i);
+
 // for(let i = 0 ; i < 3 ; i++) {
-//     console.log('DENTRO) ' + i);
+//     console.log('\tDENTRO) ' + i);
 // }
+
 // console.log('DOPO) ' + i);
+
+
+
+
+//Riassegnamento e creazione
+// var mioNumero = 5;
+// mioNumero = 7;
+
+// var mioSecondoNumero = 6;
+// var mioSecondoNumero = 7;
+
+
+// let mioNumero = 5;
+// mioNumero = 7;
+
+// let mioSecondoNumero = 6;
+// let mioSecondoNumero = 7;
+
+
+
+
+
+
+
+
+//Hoisting di let e var
+// console.log(pippo);
+// var pippo;
+// pippo = 5;
+// console.log(pippo);
+
+
+
+// console.log(pippo);
+// let pippo;
+// pippo = 5;
+// console.log(pippo);
