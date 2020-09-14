1f10846e198e597d17a77e8afc2f958f533b35d0
Alberto Bonacina <bonacina.alberto@gmail.com>
2020-09-14 19:18:53 +0200

    Rimosso codice perchè da rifare nella demo, sulla base degli appunti

--- code/test_project/routes/browser.js
+++ code/test_project/routes/browser.js
@@ -1,20 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const utils = require('../utils/request');
 
 router.get('/',(req,res,next)=>{
-
     res.render('template');
-});
-
-router.get('/randomaddresses', async (req,res,next)=>{
-
-    let risposta = await utils.getData('addresses',10);
-    let myData = risposta.data;
-
-    console.log(myData);
-    res.render('template');
-
 });
 
 module.exports = router;
--- code/test_project/utils/request.js
+++ code/test_project/utils/request.js
@@ -4,37 +4,11 @@ axios.defaults.responseType = 'json';
 axios.defaults.timeout = 1000;
 
 
-// exports.getData = (url,number)=>{
+exports.getData = (url,number)=>{
     
-//     return new Promise((resolve,reject)=>{
+    return new Promise((resolve,reject)=>{
     
-//         axios.get(`/${url}?_quantity=${number}`)
-//             .then(function (response) {
-//                 resolve(response.data);
-//             })
-//             .catch(function (error) {
-//                 reject(error);
-//             })
-//             .then(function () {
-//                 console.log('Chiamata terminata')
-//             });
-//     });
-// }
-
-
-exports.getData = (url,options) => {
-
-    const keys = Object.keys(options);
-    const values = Object.values(options);
-    
-    let arrayOptions = [];
-    for( let i = 0 ; i < keys.length ; i++ ){
-        arrayOptions.push(`${keys[i]}=${values[i]}`);    
-    }
-    let stringOptions = arrayOptions.join('&');
-
-        
-    axios.get(`/${url}?_quantity=${number}`)
+        axios.get(`/${url}?_quantity=${number}`)
             .then(function (response) {
                 resolve(response.data);
             })
@@ -44,6 +18,5 @@ exports.getData = (url,options) => {
             .then(function () {
                 console.log('Chiamata terminata')
             });
-    });    
-
+    });
 }
