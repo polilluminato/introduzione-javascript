1408a832fd26cabbdbf569bba3dc671836650bce
Alberto Bonacina <bonacina.alberto@gmail.com>
2020-09-14 22:11:12 +0200

    Rimosso codice non più utilizzato

--- code/test_project/app.js
+++ code/test_project/app.js
@@ -1,6 +1,4 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const timeout = require('connect-timeout');
 const env = require('node-env-file');
 const path = require('path');
 
@@ -9,16 +7,6 @@ env('./.env');
 
 const app = express();
 
-app.set('json spaces', 0);
-
-app.use(timeout('60s'));
-app.use(bodyParser.json({limit: '50mb'}));
-app.use(bodyParser.urlencoded({extended: true,limit: '50mb'}));
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
 
 //Aggiungo la gestione di ejs per le viste
 app.set('views', path.join(__dirname, 'views'));
